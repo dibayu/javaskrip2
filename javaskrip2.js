2f78fd1a274f2bad6fd91c5fb0cf0fbfc8c05b83
dibayu <41831791+dibayu@users.noreply.github.com>
2018-10-19 00:12:55 +0700

    Add files via upload

---
+++ javaskrip2.js
@@ -0,0 +1,7 @@
+var nama;
+ nama=prompt('Boleh tahu siapa nama anda?');
+ if(nama){
+  alert('Apa kabar '+nama+'! Senang berkenalan dengan Anda, silahkan klik "OKE" untuk melanjutkan');
+ }else{
+  alert('Hhmm sepertinya anda tidak mau berkenalan dengan saya.');
+}
